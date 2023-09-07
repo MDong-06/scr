361ba983a8fcd7160b894da9098a53bead85e90b
Minh Dong <136040904+MDong-06@users.noreply.github.com>
2023-09-07 21:17:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { readdirSync, readFileSync } = require('fs-extra');
 const path = require('path');
 try {
   var i, j;
-  const srcPath = path.join(__dirname, "/API/");
+  const srcPath = path.join(__dirname, "/public/");
   const hosting = readdirSync(srcPath).filter((file) => file.endsWith(".js"));
   for (i of hosting) {
     const { index, name } = require(srcPath + i);
@@ -15,9 +15,9 @@ try {
   // ------------------------------------------------------------------------//
   const getDirs = readdirSync(srcPath).filter((file) => !file.endsWith(".js") && !file.endsWith(".json"));
   for (const dir of getDirs) {
-    const fileName = readdirSync(path.join(__dirname, '/API/' + dir + '/')).filter((file) => file.endsWith(".js"));
+    const fileName = readdirSync(path.join(__dirname, '/public/' + dir + '/')).filter((file) => file.endsWith(".js"));
     for (j of fileName) {
-      const { index, name } = require(path.join(__dirname, '/API/' + dir + '/') + j);
+      const { index, name } = require(path.join(__dirname, '/public/' + dir + '/') + j);
       router.get(name, index);
     }
   }
